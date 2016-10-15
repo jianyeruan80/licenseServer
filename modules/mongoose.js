bcf007755dbb95015b9f3e303071647d1ce7ba85
unknown <jianyeruan@gmail.com>
2016-10-15 08:53:16 -0500

    edit port 27019

--- modules/mongoose.js
+++ modules/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var log = require('./logs');
 var dbIpAddress = process.env.MONGO_PORT_27017_TCP_ADDR || 'mongo';
-var dbPort =  '27017';
+var dbPort =  '27019';
  mongoose.connect('mongodb://' + dbIpAddress + ':' + dbPort + '/licensesDb', function(err) {
     if(err) {
         log.info('connection error', err);
