e024207614c40dee02301ab275234d2786b72552
unknown <jianyeruan@gmail.com>
2017-04-04 23:57:30 -0400

    add

--- modules/mongoose.js
+++ modules/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var log = require('./logs');
 var dbIpAddress = process.env.MONGO_PORT_27017_TCP_ADDR || 'mongo';
 var dbPort =  '27017';
- mongoose.connect('mongodb://' + dbIpAddress + ':' + dbPort + '/base', function(err) {
+ mongoose.connect('mongodb://' + dbIpAddress + ':' + dbPort + '/licenseDb', function(err) {
     if(err) {
         log.info('connection error', err);
     } else {
